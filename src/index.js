100d1eb45491cc398a4dba08229e8cd63552e0b2
Angie To <angie.to@berkeley.edu>
2018-12-30 16:34:05 -0800

    added custom middleware

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import from redux
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'; // helps inject store into components
 
 import counterReducer from './store/reducers/counter';
@@ -17,7 +17,19 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
-const store = createStore(rootReducer);
+// create a middleware w/ nested functions
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] is dispatching this action from index.js:', action);
+            const result = next(action);
+            console.log('[Middleware]\'s next/updated state:', store.getState());
+            return result;
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 // use Provider to hook the store to the application
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
